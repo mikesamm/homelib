c70be4a13c868d24b5bd6516b278cd1f7b7ad808
mike <sammartino.mike@gmail.com>
2024-10-10 10:17:44 -0500

    init frontend components

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <!-- div for the background, contain the navbar -->
+  <!-- homelib logo anchor, link to home -->
+  <!-- mobile: hamburger menu for menu -->
+  <!-- Collections to go to list of collections -->
+  <!-- avatar for account menu -->
+</template>
+
+<style>
+
+</style>
