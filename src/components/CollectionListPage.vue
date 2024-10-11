6c9dd4b189bbc8b6e6a194c1401985d7e9eb3cfd
mike <sammartino.mike@gmail.com>
2024-10-11 15:21:37 -0500

    add script, template, style boilerplate

---
+++ src/components/CollectionListPage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import CollectionList from './CollectionList.vue';
+
+</script>
+
+<template>
+  <h1>Collections</h1>
+  <CollectionList />
+</template>
+
+<style scoped>
+
+</style>
