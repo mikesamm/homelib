179e8125fdea029d11e1d0040dff4ef4c86e8bb9
mike <sammartino.mike@gmail.com>
2024-10-15 15:00:01 -0500

    modify dashboard page to LibraryHome component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import LibraryHome from './components/LibraryHome.vue';
 </script>
 
 <template>
-  <h1>📖</h1>
-  <HelloWorld msg="homelib" />
+  <NavBar id="navbar"/>
+  <!-- TODO: probably need router action in here -->
+  <LibraryHome />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
