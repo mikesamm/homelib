c52bd354dea988c9acb3e8b09de2f29a0847f815
mike <sammartino.mike@gmail.com>
2024-10-16 19:20:31 -0500

    add form for adding new collection, saves to client currently

--- src/components/CollectionList.vue
+++ src/components/CollectionList.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+let formHidden = ref(true);
+
 let id = 0;
 const collections = ref([
   { id: id++, collectionName: 'Books' },
@@ -8,13 +10,28 @@ const collections = ref([
   { id: id++, collectionName: 'Music' }
 ])
 
+const newCollectionName = ref('');
+const addNewCollection = () => {
+  collections.value.push({ id: id++, collectionName: newCollectionName.value });
+  newCollectionName.value = '';
+  formHidden.value = true;
+}
+
 </script>
 
 <template>
   <h3>Collections</h3>
+  <button id="add-collection" @click="formHidden = !formHidden">Add Collection</button>
+  <form v-if="!formHidden" @submit.prevent="addNewCollection">
+    <input
+      v-model.trim="newCollectionName"
+      placeholder="Name of new collection"
+      type="text"
+    >
+    <button>Create Collection</button>
+  </form>
   <ul>
     <li v-for="collection in collections" :key="collection.id">
-      <!-- add link to collection page, add prop on collection page for collection name, which would be collection.collectionName -->
       {{ collection.collectionName }}
     </li>
   </ul>
