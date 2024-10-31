4fb26f0f580e0a6629f35e8d3525a60123ca7197
mike <sammartino.mike@gmail.com>
2024-10-31 18:42:27 -0500

    init LibraryView page

---
+++ src/pages/LibraryView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import CollectionList from '../components/CollectionList.vue';
+
+const username = ref('TESTUSER');
+</script>
+
+<template>
+  <h1 id="library-name">
+    {{ username }}'s Library
+  </h1>
+  <!-- list of collections should go here.
+       CollectionList may be extra now that there's the CollectionView page
+       which would have the ItemList and other components that edit/build the item list -->
+  <CollectionList />
+</template>
+
+<style scoped>
+
+</style>
