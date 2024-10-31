19d1526e4428dec7524d04e19c0c2268fc943be6
mike <sammartino.mike@gmail.com>
2024-10-31 18:08:43 -0500

    create FooterNav component, style to bottom of page content

---
+++ src/components/FooterNav.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer>
+    <span>Footer goes here</span>
+  </footer>
+</template>
+
+<style>
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 2.5rem;            /* Footer height */
+}
+</style>
